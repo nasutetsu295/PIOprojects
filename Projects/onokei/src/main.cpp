ef1d5e5dd4f4088017e33c34b7158c9909120814
NasTetsu295 <sugitetu19@gmail.com>
2025-10-20 01:27:18 +0900

    add openbox

--- Projects/onokei/src/main.cpp
+++ Projects/onokei/src/main.cpp
@@ -1,13 +1,25 @@
 #include <Arduino.h>
-
+#include <Servo.h>
 
 int AnswerList[3] = {23,72,91}; // 期待する温度リスト
 int WhereNow = 0; // 期待する入力
 int margin = 10; // マージン指定 小さいほどプレイヤーに厳しい
 
+Servo myservo;  // サーボオブジェクトの作成
+myservo.attach(9);  // サーボの制御ピンを9番に設定
+
+
+
+void openbox() {
+  // 開錠処理
+  Serial.println("Box Opened!");
+  myservo.write(90); // サーボを90度に回転
+}
+
 void clear() {
   // クリア処理
   Serial.println("Game Clear!");
+  openbox();
   while (1) {
     // 無限ループで止める
   }
@@ -16,11 +28,13 @@ void clear() {
 void setup() {
   // put your setup code here, to run once:s
   Serial.begin(9600);
+  Serial.println("Game Start!");
+  myservo.write(0); // サーボを初期位置に戻す
 }
 
 void loop() {
   // put your main code here, to run repeatedly:s
-  float temp = (analogRead(4) / 1023.0) * 150; // ぽテンションメーターの値を温度に変換
+  float temp = ((analogRead(4) / 1023.0) * 120); // ぽテンションメーターの値を温度に変換
   Serial.println(temp);
   if (abs(temp - AnswerList[WhereNow]) < margin) { // 期待する温度に近いか
     Serial.println("Next temp");
